66d5ca0c6f47b032a5e27768221163def8bae221
Kelsey Newman <kpnewman2013@gmail.com>
2016-12-22 09:23:00 -0500

    Adding cars to my lot

--- src/main/java/com/theironyard/charlotte/Main.java
+++ src/main/java/com/theironyard/charlotte/Main.java
@@ -25,6 +25,11 @@ public class Main {
         listOfLots.add(new Lot(2, 25, 4, 25,  parkedCarsLot2));
         listOfLots.add(new Lot(3, 18, 7, 18, parkedCarsLot3));
 
+        parkedCarsLot0.add(new Car("Toyota", "Matrix", 2, 25, 0));
+        parkedCarsLot1.add(new Car("Hyundai", "Elantra", 1, 15, 1));
+        parkedCarsLot2.add(new Car("Toyota", "Tacoma", 3, 30, 2));
+        parkedCarsLot0.add(new Car("Chevrolet", "El Camino", 3, 45, 3));
+
         String port = System.getenv("PORT");
 
         if (port != null) {
